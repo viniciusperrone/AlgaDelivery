f7a0249309ad39c01b41858ed529b483d32703cb
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 01:37:14 -0300

    feat: created brandNew constructor on Item

--- Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Item.java
+++ Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Item.java
@@ -1,13 +1,26 @@
 package com.algaworks.algadelivery.delivery.tracking.domain.model;
 
+import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Item {
     @EqualsAndHashCode.Include
     private UUID id;
     private String name;
     private Integer quantity;
+
+    static Item brandNew(String name, Integer quantity) {
+        Item item = new Item();
+
+        item.id = UUID.randomUUID();
+        item.name = name;
+        item.quantity = quantity;
+
+        return item;
+    }
 }
