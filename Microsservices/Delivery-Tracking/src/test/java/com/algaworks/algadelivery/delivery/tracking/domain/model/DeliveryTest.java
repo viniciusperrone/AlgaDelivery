81ec49deaf31c10e0bc17ac2f7ea72b4c68d03bf
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 05:50:31 -0300

    test: created shouldChangeToPlaced and shouldNotPlaced

---
+++ Microsservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/model/DeliveryTest.java
@@ -0,0 +1,65 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.model;
+
+import com.algaworks.algadelivery.delivery.tracking.ContactPoint;
+import com.algaworks.algadelivery.delivery.tracking.domain.exceptions.DomainException;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeliveryTest {
+
+    @Test
+    public void shouldChangeToPlaced() {
+        Delivery delivery = Delivery.draft();
+
+        delivery.editPreparationDetails(createdValidPreparationDetails());
+
+        delivery.place();
+
+        assertEquals(DeliveryStatus.WAITING_FOR_COURIER, delivery.getStatus());
+        assertNotNull(delivery.getPlaceAt());
+    }
+
+    @Test
+    public void shouldNotPlaced() {
+        Delivery delivery = Delivery.draft();
+
+        assertThrows(DomainException.class, () -> {
+            delivery.place();
+        });
+
+        assertEquals(DeliveryStatus.DRAFT, delivery.getStatus());
+        assertNull(delivery.getPlaceAt());
+    }
+
+    private Delivery.PreparationDetails createdValidPreparationDetails() {
+        ContactPoint sender = ContactPoint.builder()
+                .zipCode("0000-000")
+                .street("Rua Sao Paulo")
+                .number("1000")
+                .complement("Sala 401")
+                .name("Joao Silva")
+                .phone("(11) 90000-1234")
+                .build();
+
+        ContactPoint recipient = ContactPoint.builder()
+                .zipCode("12331-342")
+                .street("Rua Brasil")
+                .number("500")
+                .complement("")
+                .name("Maria Silva")
+                .phone("(11) 91345-1332")
+                .build();
+
+        return Delivery.PreparationDetails.builder()
+                .sender(sender)
+                .recipient(recipient)
+                .distanceFee(new BigDecimal("15.00"))
+                .courierPayout(new BigDecimal("5.00"))
+                .expectedDeliveryTime(Duration.ofHours(5))
+                .build();
+    }
+}
