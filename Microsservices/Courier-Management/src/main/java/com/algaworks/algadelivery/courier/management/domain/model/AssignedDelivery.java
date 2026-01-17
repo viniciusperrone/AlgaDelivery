e4a01dedbf8a76564006adc0e7a21ab30d0767d0
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 13:25:00 -0300

    feat: added persistence in courier

--- Microsservices/Courier-Management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/AssignedDelivery.java
+++ Microsservices/Courier-Management/src/main/java/com/algaworks/algadelivery/courier/management/domain/model/AssignedDelivery.java
@@ -1,27 +1,37 @@
 package com.algaworks.algadelivery.courier.management.domain.model;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.*;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+@Entity
 @Getter
 @Setter(AccessLevel.PACKAGE)
 @NoArgsConstructor(access = AccessLevel.PACKAGE)
 @EqualsAndHashCode
 public class AssignedDelivery {
 
+    @Id
     @EqualsAndHashCode.Include
     private UUID id;
 
     private OffsetDateTime assignedAt;
 
-    static AssignedDelivery pending(UUID deliveryId) {
+    @ManyToOne(optional = false)
+    @Getter(AccessLevel.PRIVATE)
+    private Courier courier;
+
+    static AssignedDelivery pending(UUID deliveryId, Courier courier) {
         AssignedDelivery delivery = new AssignedDelivery();
 
         delivery.setId(deliveryId);
         delivery.setAssignedAt(OffsetDateTime.now());
+        delivery.setCourier(courier);
 
         return delivery;
     }
