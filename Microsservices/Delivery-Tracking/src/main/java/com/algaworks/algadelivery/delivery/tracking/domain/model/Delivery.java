70a1f2b01da6022894bbec97788c1ccdb34affcf
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 02:17:29 -0300

    feat: created items method on Delivery class

--- Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
+++ Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
@@ -51,6 +51,23 @@ public class Delivery {
         return delivery;
     }
 
+    public UUID addItem(String name, int quantity) {
+        Item item = Item.brandNew(name, quantity);
+
+        items.add(item);
+
+        return item.getId();
+
+    }
+
+    public void removeItem(UUID itemId) {
+        items.removeIf(item -> item.getId().equals(itemId))
+    }
+
+    public void removeItems() {
+        items.clear();
+    }
+
     public List<Item> getItems() {
         return Collections.unmodifiableList(this.items);
     }
