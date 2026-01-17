37e9404a18e5ebb348980a2a22e538702894a771
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 01:24:41 -0300

    feat: added EqualsAndHashCode in Item and Delivery classes

--- Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
+++ Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/model/Delivery.java
@@ -1,12 +1,17 @@
 package com.algaworks.algadelivery.delivery.tracking.domain.model;
 
+import lombok.EqualsAndHashCode;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Delivery {
+
+    @EqualsAndHashCode.Include
     private UUID id;
     private UUID courierId;
 
