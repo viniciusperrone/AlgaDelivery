dee1de07090e3fefca30ac777761cee38a386906
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 15:31:48 -0300

    test: created DeliveryRepositoryTest

---
+++ Microsservices/Delivery-Tracking/src/test/java/com/algaworks/algadelivery/delivery/tracking/domain/repository/DeliveryRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.repository;
+
+import com.algaworks.algadelivery.delivery.tracking.ContactPoint;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class DeliveryRepositoryTest {
+
+    @Autowired
+    private DeliveryRepository deliveryRepository;
+
+    @Test
+    public void shouldPersist() {
+        Delivery delivery = Delivery.draft();
+
+        delivery.editPreparationDetails(createdValidPreparationDetails());
+
+        delivery.addItem("Computador", 2);
+        delivery.addItem("Notebook", 2);
+
+        deliveryRepository.saveAndFlush(delivery);
+
+        Delivery persistedDelivery = deliveryRepository.findById(delivery.getId()).orElseThrow();
+
+        assertEquals(2, persistedDelivery.getItems().size());
+    }
+
+    private Delivery.PreparationDetails createdValidPreparationDetails() {
+        ContactPoint sender = ContactPoint.builder()
+                .zipCode("0000-000")
+                .street("Rua Sao Paulo")
+                .number("1000")
+                .complement("Sala 401")
+                .name("Joao Silva")
+                .phone("(11) 90000-1234")
+                .build();
+
+        ContactPoint recipient = ContactPoint.builder()
+                .zipCode("12331-342")
+                .street("Rua Brasil")
+                .number("500")
+                .complement("")
+                .name("Maria Silva")
+                .phone("(11) 91345-1332")
+                .build();
+
+        return Delivery.PreparationDetails.builder()
+                .sender(sender)
+                .recipient(recipient)
+                .distanceFee(new BigDecimal("15.00"))
+                .courierPayout(new BigDecimal("5.00"))
+                .expectedDeliveryTime(Duration.ofHours(5))
+                .build();
+    }
+}
