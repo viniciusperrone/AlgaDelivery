92a20a318de1a624591bc510b5d45d8cac798ac9
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 21:27:13 -0300

    feat: service injected into the controller in Delivery

--- Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
+++ Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/api/controller/DeliveryController.java
@@ -2,7 +2,9 @@ package com.algaworks.algadelivery.delivery.tracking.api.controller;
 
 import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
 import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import com.algaworks.algadelivery.delivery.tracking.domain.service.DeliveryPreparationService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,18 +12,22 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/deliveries")
+@RequiredArgsConstructor
 public class DeliveryController {
+
+    private final DeliveryPreparationService deliveryPreparationService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Delivery draft(@RequestBody @Valid DeliveryInput input) {
-        return null;
+
+        return deliveryPreparationService.draft(input);
     }
 
     @PutMapping("/{deliveryId}")
     public Delivery edit(@PathVariable UUID deliveryId,
                          @RequestBody @Valid DeliveryInput input) {
 
-        return null;
+        return deliveryPreparationService.edit(deliveryId, input);
     }
 }
