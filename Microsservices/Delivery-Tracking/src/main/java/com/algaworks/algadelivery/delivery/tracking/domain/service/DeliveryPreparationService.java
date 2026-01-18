5104b7a24e731ef355576fe3e3366378030496b9
Vinicius Perrone <perronevinicius2018@gmail.com>
2026-01-17 21:19:10 -0300

    feat: created DeliveryPreparationService

---
+++ Microsservices/Delivery-Tracking/src/main/java/com/algaworks/algadelivery/delivery/tracking/domain/service/DeliveryPreparationService.java
@@ -0,0 +1,86 @@
+package com.algaworks.algadelivery.delivery.tracking.domain.service;
+
+import com.algaworks.algadelivery.delivery.tracking.api.model.ContactPointInput;
+import com.algaworks.algadelivery.delivery.tracking.api.model.DeliveryInput;
+import com.algaworks.algadelivery.delivery.tracking.api.model.ItemInput;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.ContactPoint;
+import com.algaworks.algadelivery.delivery.tracking.domain.model.Delivery;
+import com.algaworks.algadelivery.delivery.tracking.domain.repository.DeliveryRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class DeliveryPreparationService {
+
+    private final DeliveryRepository deliveryRepository;
+
+    @Transactional
+    public Delivery draft(DeliveryInput input) {
+        Delivery delivery = Delivery.draft();
+
+        handlePreparation(input, delivery);
+
+        return deliveryRepository.saveAndFlush(delivery);
+    }
+
+    @Transactional
+    public Delivery edit(UUID deliveryId, DeliveryInput input) {
+        Delivery delivery = deliveryRepository.findById(deliveryId).
+                orElseThrow();
+
+        handlePreparation(input, delivery);
+
+        return deliveryRepository.saveAndFlush(delivery);
+    }
+
+    private void handlePreparation(DeliveryInput input, Delivery delivery) {
+        ContactPointInput senderInput = input.getSender();
+        ContactPointInput recipientInput = input.getSender();
+
+        ContactPoint sender = ContactPoint.builder()
+                .phone(senderInput.getPhone())
+                .name(senderInput.getName())
+                .complement(senderInput.getComplement())
+                .number(senderInput.getNumber())
+                .zipCode(senderInput.getZipCode())
+                .street(senderInput.getStreet())
+                .build();
+
+        ContactPoint recipient = ContactPoint.builder()
+                .phone(recipientInput.getPhone())
+                .name(recipientInput.getName())
+                .complement(recipientInput.getComplement())
+                .number(recipientInput.getNumber())
+                .zipCode(recipientInput.getZipCode())
+                .street(recipientInput.getStreet())
+                .build();
+
+
+        Duration expectedDeliveryTime = Duration.ofHours(3);
+        BigDecimal distanceFee = new BigDecimal("10");
+
+        BigDecimal payout = new BigDecimal("10");
+
+        var preparationData = Delivery.PreparationDetails.builder()
+                .recipient(recipient)
+                .sender(sender)
+                .expectedDeliveryTime(expectedDeliveryTime)
+                .courierPayout(payout)
+                .distanceFee(distanceFee)
+                .build();
+
+        delivery.editPreparationDetails(preparationData);
+
+        for (ItemInput itemInput: input.getItems()) {
+            delivery.addItem(itemInput.getName(), itemInput.getQuantity());
+        }
+
+    }
+
+}
